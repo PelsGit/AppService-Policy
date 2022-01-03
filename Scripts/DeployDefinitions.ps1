84251667da8a00c5ab4c929d059660121f6ad6ec
Rutger Pels <rutger_pels@hotmail.com>
2022-01-03 19:53:58 +0100

    dudey is stom

---
+++ Scripts/DeployDefinitions.ps1
@@ -0,0 +1,12 @@
+$armTemplateFiles = Get-ChildItem -Path C:\Local\Repos\Appservice-Policy\AppService-Policy\PolicyTemplates
+$Subscription = Get-AzSubscription -SubscriptionName 'Visual Studio Enterprise'
+
+ForEach($file in $armTemplateFiles) {
+
+        New-AzPolicyDefinition `
+        -Name (($armTemplateFiles.Name).Split(".")[0]) `
+        -Policy $file.FullName `
+        -SubscriptionId $($Subscription.Id)
+    }
+
+Get-Job | Receive-Job
