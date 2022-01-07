bd18a5097c528f7f0a4b2373615089c13ca4a3ac
Rutger Pels <rutger_pels@hotmail.com>
2022-01-08 00:27:49 +0100

    created working code for the policy definition deployment, should investigate manipulating the json file if there is time

--- Scripts/DeployDefinitions.ps1
+++ Scripts/DeployDefinitions.ps1
@@ -1,16 +1,40 @@
-$PolicyTemplateFiles = Get-ChildItem -Path C:\Repos\appservice-policy\AppService-Policy\PolicyTemplates
+$PolicyTemplateFiles = Get-ChildItem -Path PolicyTemplates
 $Subscription = Get-AzSubscription -SubscriptionName 'Visual Studio Enterprise'
 
 ForEach ($PolicyTemplateFiles in $PolicyTemplateFiles) {
+    #Check if there is a file which needs to be removed
     If ($PolicyTemplateFiles.FullName -Match 'remove-*') {
         Remove-AzPolicyDefinition `
             -Name (($PolicyTemplateFiles.Name.split("remove-")[1].Split(".")[0])) `
             -Force
     }
     else {
-        New-AzPolicyDefinition `
+        #Deploy Policy Definition
+        $Policydefinition = New-AzPolicyDefinition `
             -Name (($PolicyTemplateFiles.Name).Split(".")[0]) `
             -Policy $PolicyTemplateFiles.FullName `
             -SubscriptionId $($Subscription.Id)
+    }
+}
+
+# New policies are now deployed, time to deploy and assign the initiatives. Check if the initiative exists first, if it is: update existing policyset, if not create new and assign to correct scope.
+$Policyset = get-AzPolicySetDefinition -Name 'app-service-policy-set' -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if ($notPresent) {
+    $newPolicySetDefinition = New-AzPolicySetDefinition `
+        -Name 'app-service-policy-set' `
+        -PolicyDefinition "policyset\policyset.json"
+
+    New-AzPolicyAssignment `
+        -Name 'AppServicePolicyAssignment' `
+        -PolicyDefinition $newPolicySetDefinition `
+        -Scope $Subscription `
+        -AssignIdentity `
+        -Location 'west europe'
+
+    else {
+        set-AzPolicySetDefinition `
+            -Name $Policyset.Name `
+            -PolicyDefinition "policyset\policyset.json"
     }
 }
