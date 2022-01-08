982445cd1a4b891c77fa5f79019b12f49553c603
Rutger Pels <rutger_pels@hotmail.com>
2022-01-08 21:39:25 +0100

    updating remove-whatif script to display the amount of definitions to be added or removed

--- Scripts/Remove-whatif.ps1
+++ Scripts/Remove-whatif.ps1
@@ -3,14 +3,16 @@ $PolicyTemplateFiles = Get-ChildItem -Path PolicyTemplates\policydefinitions
 ForEach ($PolicyTemplateFiles in $PolicyTemplateFiles) {
     #Check if there is a file which needs to be removed
     If ($PolicyTemplateFiles.FullName -Match 'remove-*') {
+        $PolicytobeDeleted = (($PolicyTemplateFiles.Name.split("remove-")[1].Split(".")[0]))
+        $tobedeleted = $PolicytobeDeleted.count
         Remove-AzPolicyDefinition `
-            -Name (($PolicyTemplateFiles.Name.split("remove-")[1].Split(".")[0])) `
+            -Name $PolicytobeDeleted `
             -WhatIf
-        Write-host "Removing policies, please check"
+            Write-host "$tobedeleted Policy Definition(s) to be deleted"
     }
     else {
         $PolicyTemplateFiles = Get-ChildItem -Path PolicyTemplates\policydefinitions -Exclude 'remove-*'
         $tobeadded = $PolicyTemplateFiles.count
-        Write-host "$tobeadded Policy Definition to be updated or created"
+        Write-host "$tobeadded Policy Definition(s) to be updated or created"
     }
 }
