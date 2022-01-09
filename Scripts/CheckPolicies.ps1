150026dad6773f8ed669e2bf54baf0d58924811b
Rutger Pels <rutger_pels@hotmail.com>
2022-01-09 01:20:01 +0100

    finalizing pipeline for policy deployments

--- Scripts/CheckPolicies.ps1
+++ Scripts/CheckPolicies.ps1
@@ -4,15 +4,21 @@ ForEach ($PolicyTemplateFiles in $PolicyTemplateFiles) {
     #Check if there is a file which needs to be removed
     If ($PolicyTemplateFiles.FullName -Match 'remove-*') {
         $PolicytobeDeleted = (($PolicyTemplateFiles.Name.split("remove-")[1].Split(".")[0]))
-        $tobedeleted = $PolicytobeDeleted.count
+        $PolicytobeDeletedName = $PolicytobeDeleted
         Remove-AzPolicyDefinition `
             -Name $PolicytobeDeleted `
             -WhatIf
-            Write-host "$tobedeleted Policy Definition(s) to be deleted"
     }
     else {
         $PolicyTemplateFiles = Get-ChildItem -Path PolicyTemplates\policydefinitions -Exclude 'remove-*'
-        $tobeadded = $PolicyTemplateFiles.count
-        Write-host "$tobeadded Policy Definition(s) to be updated or created"
+        $PolicyTemplateFilesName = $PolicyTemplateFiles.Name
     }
+}
+
+Write-host "$PolicyTemplateFilesName will be created or updated"
+if($PolicyTemplateFiles -eq $null) {
+Write-host "No Policies will be deleted"
+}
+else {
+    Write-host "$PolicytobeDeleted will be deleted"
 }
